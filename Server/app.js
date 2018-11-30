7866b4b7c848cfb232937575472f98a93da57f44
igor <cool.rainbow2012@yandex.ru>
2018-11-30 15:43:18 +0300

    Старт разработки

---
+++ Server/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.render('index.pug', {title: 'Заголовок', message: 'Сообщение'});
+});
+
+app.get('/igor', function (req, res) {
+   res.send('Igor');
+});
+
+var server = app.listen(3000, function (req, res) {
+    var addres = server.address();
+    console.log("Server starting: %s:%d;", "127.0.0.1", addres.port);
+});
